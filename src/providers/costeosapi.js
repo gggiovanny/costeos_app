c18ca9d50a4968fa3225d4e5f9c9f48b5ca20ee7
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-03-07 23:51:13 -0600

    Agregado cambio de api automatico dependiendo si se está en producción o en desarrollo.

--- src/providers/costeosapi.js
+++ src/providers/costeosapi.js
@@ -1,5 +1,12 @@
-import axios from 'axios';
+import axios from 'axios'
+
+const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? process.env.REACT_APP_DEV_API_URL
+    : process.env.REACT_APP_PROD_API_URL
+
+console.log(API_URL)
 
 export default axios.create({
-  baseURL: 'https://costeosapi.herokuapp.com/'
-});
+  baseURL: API_URL,
+})
