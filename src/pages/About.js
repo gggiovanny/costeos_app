8a78b6899d64ad33a3a4edcdc4333fd40c6115c4
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-03-31 17:01:38 -0600

    Agregado custom favicon

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,18 @@ export class About extends Component {
   render() {
     return (
       <div>
-        <h1 className='title'>Acerca de</h1>
-        <p>App creada por Giovanny González Baltazar usando las ideas de Kora Amaya Valdéz.</p>
+        <h1 className="title">Acerca de</h1>
+        <p>
+          App creada por Giovanny González Baltazar usando las ideas de Kora
+          Amaya Valdéz.
+        </p>
+        <p>
+          Favicon creado por{' '}
+          <a href="https://www.flaticon.com/authors/iconixar">iconixar</a> en{' '}
+          <a href="https://www.flaticon.com/free-icon/chef_2413311?term=recipe&page=1&position=20&page=1&position=20&related_id=2413311">
+            flaticon.com
+          </a>
+        </p>
       </div>
     )
   }
